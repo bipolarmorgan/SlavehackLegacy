45018d5fbd274c6de0b2d3c11463c6a29598c53f
Christian Medina <cmedina3@angelo.edu>
2014-07-09 15:35:25 -0500

    Fixes added to mailbot.

--- smtp/Send_Mail.php
+++ smtp/Send_Mail.php
@@ -4,26 +4,30 @@
 
 <?php
 	function Send_Mail($to, $subject, $body){
-		require 'class.phpmailer.php';
-		$from = "Slavehack: Legacy Mailbot";
-		$mail = new PHPMailer();
-		$mail -> IsSMTP();
-		$mail -> SMTPSecure = 'ssl';
-		$mail -> IsHTML(true);
-		$mail -> SMTPAuth = true;
-		$mail -> Host = "smtp.gmail.com";
-		$mail -> Port = 465;
-		$mail -> Username = "SlavehackLegacyMailbot@gmail.com";
-		$mail -> Password = "YVtYrqkMbxABsWm8";
-		$mail -> SetFrom($from, 'Slavehack Legacy Mailbot');
-		$mail -> AddReplyTo($from, 'Slavehack Legacy Mailbot');
-		$mail -> Subject = $subject;
-		$mail -> MsgHTML($body);
-		$address = $to;
-		$mail -> AddAddress($address, $to);
-		if(!$mail->Send()){
-	    echo "Mailer Error: " . $mail->ErrorInfo;
-	    }
-	    else{ echo "Mailing successful. "; }
+		try {
+			require 'class.phpmailer.php';
+			$from = "donotreply <SlavehackLegacyMailbot@gmail.com>";
+			$mail = new PHPMailer();
+			$mail -> IsSMTP();
+			$mail -> SMTPSecure = 'ssl';
+			$mail -> IsHTML(true);
+			$mail -> SMTPAuth = true;
+			$mail -> Host = "smtp.gmail.com";
+			$mail -> Port = 465;
+			$mail -> Username = "SlavehackLegacyMailbot@gmail.com";
+			$mail -> Password = "YVtYrqkMbxABsWm8";
+			$mail -> From = "SlavehackLegacyMailbot@gmail.com";
+			$mail -> FromName = "donotreply";
+			$mail -> Subject = $subject;
+			$mail -> MsgHTML($body);
+			$address = $to;
+			$mail -> AddAddress($address, $to);
+			if(!$mail->Send()){
+		    echo "Mailer Error: " . $mail->ErrorInfo;
+		    }
+		    else{}
+		} catch (phpmailerException $e) {
+			echo $e->getMessage();
+		}
 	}
 ?>
