f9dbf3611c1bb1a4cf568e89bcce3202d8e7f56a
Christian Medina <cmedina3@angelo.edu>
2014-07-30 16:25:00 -0500

    Updates to help command for terminal.

--- game/internet.php
+++ game/internet.php
@@ -72,12 +72,12 @@
 				if (line == 'exit') this.close()
 				//else this.type('You typed: '+line);
 				else if (line == 'help') {
-					this.type('For info on a specific command, type: help [COMMAND].\r\n' +
-						'bin2hex [STRING] \t Converts a binary string to hexadecimal.\r\n' +
-						'hex2bin [STRING] \t Converts a hexadecimal string to binary.\r\n' +
-						'pulse \t Dumps an amount of data containing an encrypted password.\r\n' +
-						'base64_decode [STRING] \t Decodes a string encoded with MIME base64.\r\n' +
-						'mutilate [STRING] \t Utilizes WaterWall software to crack a CHA-encrypted string.\r\n' +
+					this.type('For info on a specific command, type: help [COMMAND].\n' +
+						'bin2hex [STRING] \t Converts a binary string to hexadecimal.\n' +
+						'hex2bin [STRING] \t Converts a hexadecimal string to binary.\n' +
+						'pulse \t Dumps an amount of data containing an encrypted password.\n' +
+						'base64_decode [STRING] \t Decodes a string encoded with MIME base64.\n' +
+						'mutilate [STRING] \t Utilizes WaterWall software to crack a CHA-encrypted string.\n' +
 						'exit \t Exits the terminal.');
 				}
 			}
