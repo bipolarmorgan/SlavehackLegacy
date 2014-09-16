b33a8758bd286bc65de8aca4c2ff4330093120d6
navybofus <cford03@hotmail.com>
2014-09-16 02:57:25 -0500

    Testing Logout

--- logout.php
+++ logout.php
@@ -6,6 +6,8 @@
     unset($_SESSION['ip']);
     unset($_SESSION['pass']);
 
+    header("refresh:5;url=index.php");
+
     echo "
     <html>
     <body bgcolor='black' style='color: #FFFFFF; text-align: center;'>
@@ -13,5 +15,4 @@ You have successfully logged out of Slavehack: Legacy<br />
 <a href='index.php'>Click here if you don't want to wait.</a>
     </body>
     </html>";
-header("refresh:5;url=index.php");
 ?>
