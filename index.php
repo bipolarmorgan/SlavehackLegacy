190754322089a64d6092f542d949eff2edefa784
Christian Medina <cmedina3@angelo.edu>
2014-09-18 15:02:12 -0500

    Updates for cookie authorization.

--- index.php
+++ index.php
@@ -18,9 +18,12 @@
 <?php
     include("page_parts.php");
 
+    echo("Check 0");
+
     //COOKIE CHECK//
 
     if(isset($_COOKIE['auth'])){
+        echo("Check 1");
         $clean = array();
         $mysqli = array();
 
@@ -40,10 +43,13 @@
                 FROM users
                 WHERE identifier = '{$mysqli['identifier']}'";
 
+        echo("Check 2");
+
         if ($result = mysqli_query($link, $qry)){
+            echo("Check 3");
             if(mysqli_num_rows($result)){
                 $record = mysqli_fetch_assoc($result);
-
+                echo("Check 4");
                 if($clean['token'] != $record['token']){
                     echo("Invalid token.");
                 }
